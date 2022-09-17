9123c7cf507f36a662a95397f0ef206be854f99c
Jacob3251 <jacobfrye3251@gmail.com>
2022-09-17 14:25:31 +0600

    all backend done for posting and deleting. updating and deletion remaining

--- index.js
+++ index.js
@@ -25,6 +25,19 @@ async function run() {
     const user = { email: "cse_1832020032@lus.ac.bd" };
 
     const userCollection = client.db("UserList").collection("users");
+    const transportCollection = client
+      .db("TransportNotice")
+      .collection("notices");
+    const GeneralPostCollection = client.db("GeneralPosts").collection("posts");
+    const selectedPostUniversityCollection = client
+      .db("SelectedPostList")
+      .collection("universitypost");
+    const selectedPostDepartmentCollection = client
+      .db("SelectedPostList")
+      .collection("departmentpost");
+    const selectedPostTeacherCollection = client
+      .db("SelectedPostList")
+      .collection("teacherpost");
     const studentCollection = client.db("UserList").collection("student");
     const teacherCollection = client.db("UserList").collection("teacher");
     // console.log(`user has been added to database ${result.insertedId}`);
@@ -55,6 +68,84 @@ async function run() {
       //   const result = await userCollection.insertOne(newUser);
       //   res.send({ result: "success", newUser });
     });
+    //showing selected posts in department route ********************* statically added
+    app.post("/selectedpost", async (req, res) => {
+      //  ====================== can be used to dynamically add data(start)
+      // ============================== static part
+
+      const newPost = req.body;
+      if (newPost.postType === 0) {
+        const result = await selectedPostUniversityCollection.insertOne(
+          newPost
+        );
+        res.send(result);
+      } else if (newPost.postType === 1) {
+        const result = await selectedPostDepartmentCollection.insertOne(
+          newPost
+        );
+        res.send(result);
+      } else {
+        const result = await selectedPostTeacherCollection.insertOne(newPost);
+        res.send(result);
+      }
+
+      console.log("Added new post: ", newPost);
+      // const result1 = await selectedPostDepartmentCollection.insertOne({
+      //   newPostDepartment,
+      // });
+      // const result2 = await selectedPostTeacherCollection.insertOne({
+      //   newPostTeacher,
+      // });
+      // ===============================================================================
+    });
+    // ============================ have to be uncommented to dynacally add data(end)
+    app.post("/transportnotice", async (req, res) => {
+      const newNotice = req.body;
+      const result = await transportCollection.insertOne(newNotice);
+      res.send(result);
+      console.log("Added new transport notice", newNotice);
+    });
+    app.get("/transportnotice", async (req, res) => {
+      const query = {};
+      const cursor = transportCollection.find(query);
+      const notices = await cursor.toArray();
+      res.send(notices);
+    });
+    app.post("/generalposts", async (req, res) => {
+      const newPost = req.body;
+      const result = await GeneralPostCollection.insertOne(newPost);
+      res.send(result);
+      console.log("Added new post: ", newPost);
+    });
+    app.get("/generalposts", async (req, res) => {
+      const query = {};
+      const cursor = GeneralPostCollection.find(query);
+      const generalposts = await cursor.toArray();
+      res.send(generalposts);
+    });
+
+    app.get("/selectedpost", async (req, res) => {
+      const query = {};
+      const cursor = selectedPostUniversityCollection.find(query);
+      const cursor1 = selectedPostDepartmentCollection.find(query);
+      const cursor2 = selectedPostTeacherCollection.find(query);
+      const universitypost = await cursor.toArray();
+      const departmentpost = await cursor1.toArray();
+      const teacherpost = await cursor2.toArray();
+      const all = [universitypost, departmentpost, teacherpost];
+
+      res.send(all);
+    });
+    app.get("/users", async (req, res) => {
+      const query = {};
+      const cursor = studentCollection.find(query);
+      const cursor1 = teacherCollection.find(query);
+      const studentList = await cursor.toArray();
+      const teacherList = await cursor1.toArray();
+      const all = [studentList, teacherList];
+
+      res.send(all);
+    });
   } finally {
     // client.close();
   }
@@ -62,7 +153,7 @@ async function run() {
 
 run().catch(console.dir);
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
   res.send("Welcome to the server side of LU Community Forum");
 });
 
